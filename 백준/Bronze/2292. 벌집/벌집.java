fef0e65a1191899fe9d79525ae1cbf063838e272
DooHyoJeong <83206790+DooHyoJeong@users.noreply.github.com>
2025-05-06 19:26:51 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -4,18 +4,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        int start = 1;
-        int count = 0;
-        while(n > 0){
-            n-=start;
+        int count = 1;
+        int max = 1;
+        while(n > max){
+            max+=6*count;
             count++;
-            if(start == 1){
-                start +=5;
-            }else{
-                start += 6;
-            }
-
         }
+//        int start = 1;
+//        int count = 0;
+//        while(n > 0){
+//            n-=start;
+//            count++;
+//            if(start == 1){
+//                start +=5;
+//            }else{
+//                start += 6;
+//            }
+//
+//        }
         System.out.println(count);
     }
 }
