a8aa2fbcbb361ba8e7e534c2cc36ac0719b0fac6
DooHyoJeong <83206790+DooHyoJeong@users.noreply.github.com>
2025-05-08 00:04:35 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -23,23 +23,19 @@ public class Main {
                 for (int k = j+1; k < n_list.length;k++){
                     int sum =0;
                     sum += (n_list[i])+(n_list[j])+(n_list[k]);
-                    if(sum > M){
-                        continue;
+
+                    if(sum == M){
+                        System.out.println(sum);
+                        return;
                     }
-                    else if(Math.abs(sum-M) < Math.abs(candidate-M)){
+                    if(sum < M && sum > candidate){
 //                        System.out.println("*바꿈*");
-                        candidate = sum;
+                          candidate = sum;
 //                        System.out.println(sum);
 //                        System.out.println(candidate);
 
-                    }else if(sum-M == 0){
-                        break;
                     }
-                    else{
-//                        System.out.println("*안바꿈*");
-//                        System.out.println(sum);
-//                        System.out.println(candidate);
-                    }
+
                 }
             }
         }
