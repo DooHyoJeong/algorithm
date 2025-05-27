c6fb19c63f4492c6566c8d6359f0d8449cd4701f
DooHyoJeong <83206790+DooHyoJeong@users.noreply.github.com>
2025-05-27 14:54:21 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int i : array){
+            if(i > height){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
